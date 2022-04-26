078790e04fa6d89993d2d2fec5ca684ccdcaea59
JustWaveThings <flove101@gmail.com>
2022-04-26 14:52:25 -0400

    added event listener for form fill and validation classes if the input matches the regex for the specific field.

--- validation.js
+++ validation.js
@@ -1,8 +1,26 @@
 // validation script here
 var reg = /[a-z]/gi;
-    
+
+const inputs = document.querySelectorAll('input');
+
 const patterns = {
     telephone: /^\d{10}$/
-
-    
 };
+
+// validation function
+
+function validate(field, regex) {
+   // console.log(regex.test(field.value));
+    if (regex.test(field.value)) {
+        field.className = 'valid';
+    } else {
+        field.className = 'invalid';
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("keyup", (e) => {
+       // console.log(e.target.attributes.name.value);
+        validate(e.target, patterns['telephone']);
+  })
+}) 
