abfd3d0278f50f8e9ef0d3a55f4696b01e9b44cb
JustWaveThings <flove101@gmail.com>
2022-04-27 10:59:04 -0400

    finished regex part of tutorial with email validation correct. Loved this

--- validation.js
+++ validation.js
@@ -5,9 +5,10 @@ const inputs = document.querySelectorAll('input');
 
 const patterns = {
   telephone: /^\d{10}$/,
-  username: /^[a-z\d]{5,12}$/,
+  username: /^[a-z\d]{5,12}$/i,
   password: /^[\w@-]{8,20}$/,
   slug: /^[a-z\d]{8,20}$/,
+  email: /^([[a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 };
 
 // validation function
